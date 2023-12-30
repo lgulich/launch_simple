1a1fc190380060c90b6b76c1782b376e4aec0879
lgulich <lionel.gulich@gmail.com>
2023-12-30 17:39:25 +0100

    Cleanup CI

--- launch_simple/doc/conf.py
+++ launch_simple/doc/conf.py
@@ -1,15 +1,13 @@
 """Configuration file for the Sphinx documentation builder."""
 # pylint: disable=invalid-name
-import sys
 
 # -- Project information -----------------------------------------------------
 project = 'launch_simple'
 copyright = '2023, Lionel Gulich'  #pylint: disable=redefined-builtin
 author = 'Lionel Gulich'
-release = '0.1.0'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
-sys.path += ['launch_simple']
 extensions = [
     'myst_parser',
     'sphinx.ext.autodoc',
@@ -33,7 +31,6 @@ root_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-# html_logo = '_static/ascento_logo_white_5000x2000.png'
 
 html_context = {
     'display_github': True,
